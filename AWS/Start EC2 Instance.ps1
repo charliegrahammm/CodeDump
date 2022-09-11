fb6a056e0f8f5b8c2639b3b855b98be20a77d8d2
Charlie Graham <charliegrahamm2014@gmail.com>
2022-09-11 22:47:11 +0100

    deleted:    AWS/Start EC2 Instance 	new file:   AWS/Start EC2 Instance.ps1 	new file:   Misc/Self Elevate.ps1

---
+++ AWS/Start EC2 Instance.ps1
@@ -0,0 +1,21 @@
+# This will self elevate the script with a UAC prompt since this script needs to be run as an Administrator in order to function properly.
+If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]'Administrator')) {
+    Write-Host "You didn't run this script as an Administrator. This script will self elevate to run as an Administrator and continue." -ForegroundColor White -BackgroundColor Red 
+    Start-Sleep 1
+    Write-Host " Launching in Admin mode" -ForegroundColor White -BackgroundColor Green
+    $pwshexe = (Get-Command 'powershell.exe').Source
+    Start-Process $pwshexe -ArgumentList ("-NoProfile -ExecutionPolicy Bypass -File `"{0}`"" -f $PSCommandPath) -Verb RunAs
+    Exit
+}
+
+# Install Module if not detected
+if (Get-Module -ListAvailable -Name SomeModule) {
+    Write-Host "Module exists" -ForegroundColor White -BackgroundColor Green
+} 
+else {
+    Write-Host "Module does not exist, installing..." -ForegroundColor White -BackgroundColor Red 
+    Install-Module -Name AWS.Tools.EC2
+}
+
+# List instances Available
+Write-Host "Listing available Instances"
---
+++ Misc/Self Elevate.ps1
@@ -0,0 +1,9 @@
+# This will self elevate the script with a UAC prompt since this script needs to be run as an Administrator in order to function properly.
+If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]'Administrator')) {
+    Write-Host "You didn't run this script as an Administrator. This script will self elevate to run as an Administrator and continue."
+    Start-Sleep 1
+    Write-Host " Launching in Admin mode" -f DarkRed
+    $pwshexe = (Get-Command 'powershell.exe').Source
+    Start-Process $pwshexe -ArgumentList ("-NoProfile -ExecutionPolicy Bypass -File `"{0}`"" -f $PSCommandPath) -Verb RunAs
+    Exit
+}
