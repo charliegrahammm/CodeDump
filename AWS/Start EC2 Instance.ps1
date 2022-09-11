e38ab4738d4c115e46fa630db48310fc5861d37f
Charlie Graham <charliegrahamm2014@gmail.com>
2022-09-11 22:48:52 +0100

    modified:   AWS/Start EC2 Instance.ps1

--- AWS/Start EC2 Instance.ps1
+++ AWS/Start EC2 Instance.ps1
@@ -2,7 +2,7 @@
 if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]'Administrator')) {
     Write-Host "You didn't run this script as an Administrator. This script will self elevate to run as an Administrator and continue." -ForegroundColor White -BackgroundColor Red 
     Start-Sleep 1
-    Write-Host " Launching in Admin mode" -ForegroundColor White -BackgroundColor Green
+    Write-Host "Launching in Admin mode" -ForegroundColor White -BackgroundColor Green
     $pwshexe = (Get-Command 'powershell.exe').Source
     Start-Process $pwshexe -ArgumentList ("-NoProfile -ExecutionPolicy Bypass -File `"{0}`"" -f $PSCommandPath) -Verb RunAs
     Exit
