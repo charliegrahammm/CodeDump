9cf490a45bdc819e21c544f8594ca0a9ceb07999
Charlie Graham <charliegrahamm2014@gmail.com>
2022-09-11 22:48:27 +0100

    modified:   AWS/Start EC2 Instance.ps1

--- AWS/Start EC2 Instance.ps1
+++ AWS/Start EC2 Instance.ps1
@@ -1,5 +1,5 @@
 # This will self elevate the script with a UAC prompt since this script needs to be run as an Administrator in order to function properly.
-If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]'Administrator')) {
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]'Administrator')) {
     Write-Host "You didn't run this script as an Administrator. This script will self elevate to run as an Administrator and continue." -ForegroundColor White -BackgroundColor Red 
     Start-Sleep 1
     Write-Host " Launching in Admin mode" -ForegroundColor White -BackgroundColor Green
