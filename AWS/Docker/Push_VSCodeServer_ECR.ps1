24cbc8f6f2bbe958cad20e5f317a0759ace06646
Charlie Graham <charliegrahamm2014@gmail.com>
2022-09-17 08:03:49 +0100

    new file:   AWS/AWS-CLI_Docker.ps1 	new file:   AWS/Docker/Push_VSCodeServer_ECR.ps1

---
+++ AWS/Docker/Push_VSCodeServer_ECR.ps1
@@ -0,0 +1,34 @@
+<# 
+ ██████╗██╗  ██╗ █████╗ ██████╗ ██╗     ██╗███████╗     ██████╗ ██████╗  █████╗ ██╗  ██╗ █████╗ ███╗   ███╗
+██╔════╝██║  ██║██╔══██╗██╔══██╗██║     ██║██╔════╝    ██╔════╝ ██╔══██╗██╔══██╗██║  ██║██╔══██╗████╗ ████║
+██║     ███████║███████║██████╔╝██║     ██║█████╗      ██║  ███╗██████╔╝███████║███████║███████║██╔████╔██║
+██║     ██╔══██║██╔══██║██╔══██╗██║     ██║██╔══╝      ██║   ██║██╔══██╗██╔══██║██╔══██║██╔══██║██║╚██╔╝██║
+╚██████╗██║  ██║██║  ██║██║  ██║███████╗██║███████╗    ╚██████╔╝██║  ██║██║  ██║██║  ██║██║  ██║██║ ╚═╝ ██║
+ ╚═════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝╚═╝╚══════╝     ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝     ╚═╝
+
+.SYNOPSIS  
+    This script will pull the latest linuxserver/code-server docker image and push it to AWS ECR.
+.DESCRIPTION  
+    Pulls the latest linuxserver/code-server docker image from docker hub and pushes it to AWS ECR
+.NOTES  
+    File Name  : ScriptName.ps1  
+    Author     : Charlie Graham 
+    Requires   : PowerShell V2, AWS CLI, Docker
+#>
+
+# This will self elevate the script with a UAC prompt since this script needs to be run as an Administrator in order to function properly.
+If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]'Administrator')) {
+    Write-Host "You didn't run this script as an Administrator. This script will self elevate to run as an Administrator and continue."
+    Start-Sleep 1
+    Write-Host " Launching in Admin mode" -f DarkRed
+    $pwshexe = (Get-Command 'powershell.exe').Source
+    Start-Process $pwshexe -ArgumentList ("-NoProfile -ExecutionPolicy Bypass -File `"{0}`"" -f $PSCommandPath) -Verb RunAs
+    Exit
+}
+
+# Pull latest docker image
+docker pull linuxserver/code-server
+
+
+
+aws ecr get-login-password --region region | docker login --username AWS --password-stdin aws_account_id.dkr.ecr.region.amazonaws.com
