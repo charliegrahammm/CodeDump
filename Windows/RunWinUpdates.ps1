6ada7d24f2b8762afe79d2aca9319d7a6ccf885d
Charlie Graham <charliegrahamm2014@gmail.com>
2022-09-22 13:40:38 +0100

    new file:   Windows/RunWinUpdates.ps1

---
+++ Windows/RunWinUpdates.ps1
@@ -0,0 +1,41 @@
+<# 
+ ██████╗██╗  ██╗ █████╗ ██████╗ ██╗     ██╗███████╗     ██████╗ ██████╗  █████╗ ██╗  ██╗ █████╗ ███╗   ███╗
+██╔════╝██║  ██║██╔══██╗██╔══██╗██║     ██║██╔════╝    ██╔════╝ ██╔══██╗██╔══██╗██║  ██║██╔══██╗████╗ ████║
+██║     ███████║███████║██████╔╝██║     ██║█████╗      ██║  ███╗██████╔╝███████║███████║███████║██╔████╔██║
+██║     ██╔══██║██╔══██║██╔══██╗██║     ██║██╔══╝      ██║   ██║██╔══██╗██╔══██║██╔══██║██╔══██║██║╚██╔╝██║
+╚██████╗██║  ██║██║  ██║██║  ██║███████╗██║███████╗    ╚██████╔╝██║  ██║██║  ██║██║  ██║██║  ██║██║ ╚═╝ ██║
+ ╚═════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝╚═╝╚══════╝     ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝     ╚═╝
+
+.SYNOPSIS  
+    This script automatically runs Windows Updates. 
+.DESCRIPTION  
+    This script will automatically install any necessary modules, pull down a list of required updates from Microsoft's servers and install them. It may need to be ran multiple times.
+.NOTES  
+    File Name  : RunWinUpdates.ps1  
+    Author     : Charlie Graham 
+    Requires   : PowerShell v2, PSWindowsUpdate
+#>
+
+# This will self elevate the script with a UAC prompt since this script needs to be run as an Administrator in order to function properly.
+If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]'Administrator')) {
+    Write-Host "You didn't run this script as an Administrator. This script will self elevate to run as an Administrator and continue."
+    Start-Sleep 1
+    Write-Host " Launching in Admin mode" -f DarkRed
+    $pwshexe = (Get-Command 'powershell.exe').Source
+    Start-Process $pwshexe -ArgumentList ("-NoProfile -ExecutionPolicy Bypass -File `"{0}`"" -f $PSCommandPath) -Verb RunAs
+    Exit
+}
+
+# Install PSWindowsUpdate if not already
+if (Get-Module -ListAvailable -Name PSWindowsUpdate) {
+    Write-Host "PSWindowsUpdate Module exists" -ForegroundColor Green
+    Import-Module PSWindowsUpdate
+} 
+else {
+    Write-Host "PSWindowsUpdate does not exist" -ForegroundColor Red
+    Install-Module -Name PSWindowsUpdate
+    Import-Module PSWindowsUpdate
+}
+
+# Run Windows Updates and reboot automatically
+Install-WindowsUpdate -AcceptAll -AutoReboot
