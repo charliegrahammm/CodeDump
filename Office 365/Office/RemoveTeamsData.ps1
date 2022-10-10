c17be6b3d7d05f5f9c92eddedb37e79b242e6fe9
charliegrahamm <93210017+charliegrahamm@users.noreply.github.com>
2022-10-10 16:15:01 +0100

    new file:   Office 365/Office/RemoveTeamsData.ps1

---
+++ Office 365/Office/RemoveTeamsData.ps1
@@ -0,0 +1,27 @@
+<# 
+ ██████╗██╗  ██╗ █████╗ ██████╗ ██╗     ██╗███████╗     ██████╗ ██████╗  █████╗ ██╗  ██╗ █████╗ ███╗   ███╗
+██╔════╝██║  ██║██╔══██╗██╔══██╗██║     ██║██╔════╝    ██╔════╝ ██╔══██╗██╔══██╗██║  ██║██╔══██╗████╗ ████║
+██║     ███████║███████║██████╔╝██║     ██║█████╗      ██║  ███╗██████╔╝███████║███████║███████║██╔████╔██║
+██║     ██╔══██║██╔══██║██╔══██╗██║     ██║██╔══╝      ██║   ██║██╔══██╗██╔══██║██╔══██║██╔══██║██║╚██╔╝██║
+╚██████╗██║  ██║██║  ██║██║  ██║███████╗██║███████╗    ╚██████╔╝██║  ██║██║  ██║██║  ██║██║  ██║██║ ╚═╝ ██║
+ ╚═════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝╚═╝╚══════╝     ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝     ╚═╝
+
+.SYNOPSIS  
+    This script removes all Microsoft Teams data from a machine.
+.DESCRIPTION  
+    Removes all Microsoft Teams data from a machine. Must be ran as the logged in user. It is recommended to reboot before and after this script has ran.
+.NOTES  
+    File Name  : RemoveTeamsData.ps1  
+    Author     : Charlie Graham 
+    Requires   : PowerShell v2
+#>
+
+# Get users
+Write-Output "Gathering Users"
+$users = Get-ChildItem -Path "C:\Users"
+
+# Loop through users and delete the file
+Write-Output "Deleting Teams Data"
+$users | ForEach-Object {
+    Remove-Item -Path "C:\Users\$($_.Name)\AppData\Local\Microsoft\Teams" -Force -Recurse -ErrorAction Ignore
+}
