4de620147039596d6cb842cf3dfb44eb668e8374
charliegrahamm <93210017+charliegrahamm@users.noreply.github.com>
2022-10-10 13:08:56 +0100

    modified:   Office 365/Office/RemoveOutlookProfiles.ps1

--- Office 365/Office/RemoveOutlookProfiles.ps1
+++ Office 365/Office/RemoveOutlookProfiles.ps1
@@ -11,7 +11,7 @@
 .DESCRIPTION  
     Removes all outlook profiles for the logged in user. Must be ran as the logged in user. It is recommended to reboot before and after this script has ran.
 .NOTES  
-    File Name  : RemoveOutlookProfiles.ps1  
+    File Name  : RemoveOutlookProfiles.ps1  Black
     Author     : Charlie Graham 
     Requires   : PowerShell v2
 #>
@@ -24,21 +24,21 @@ $Key15 = "HKCU:\SOFTWARE\Microsoft\Office\15.0\Outlook\Profiles"
 
 # Kill Office Applications
 Stop-Process -Name OUTLOOK -Force -EA SilentlyContinue
-Write-Host "Outlook is closing..." -BackgroundColor Green -ForegroundColor Black
+Write-Output "Outlook is closing..." -BackgroundColor Green -ForegroundColor Black
 Stop-Process -Name EXCEL -Force -EA SilentlyContinue
-Write-Host "Excel is closing..." -BackgroundColor Green -ForegroundColor Black
+Write-Output "Excel is closing..." -BackgroundColor Green -ForegroundColor Black
 Stop-Process -Name WINWORD -Force -EA SilentlyContinue
-Write-Host "Word is closing..." -BackgroundColor Green -ForegroundColor Black
+Write-Output "Word is closing..." -BackgroundColor Green -ForegroundColor Black
 Stop-Process -Name POWERPNT -Force -EA SilentlyContinue
-Write-Host "Powerpoint is closing..." -BackgroundColor Green -ForegroundColor Black
+Write-Output "Powerpoint is closing..." -BackgroundColor Green -ForegroundColor Black
 Stop-Process -Name VISIO -Force -EA SilentlyContinue
-Write-Host "Visio is closing..." -BackgroundColor Green -ForegroundColor Black
+Write-Output "Visio is closing..." -BackgroundColor Green -ForegroundColor Black
 Stop-Process -Name ONENOTE -Force -EA SilentlyContinue
-Write-Host "OneNote is closing..." -BackgroundColor Green -ForegroundColor Black
+Write-Output "OneNote is closing..." -BackgroundColor Green -ForegroundColor Black
 Stop-Process -Name MSPUB -Force -EA SilentlyContinue
-Write-Host "Publisher is closing..." -BackgroundColor Green -ForegroundColor Black
+Write-Output "Publisher is closing..." -BackgroundColor Green -ForegroundColor Black
 Stop-Process -Name MSACCESS -Force -EA SilentlyContinue
-Write-Host "Access is closing..." -BackgroundColor Green -ForegroundColor Black
+Write-Output "Access is closing..." -BackgroundColor Green -ForegroundColor Black
 
 # Locate data files
 $TestPath = Test-Path -Path $Path\*.ost
@@ -49,7 +49,7 @@ if ($TestPath -eq $TRUE) {
     Remove-Item $GciPath -EA SilentlyContinue
 }
 else {
-    Write-Host ".OST not found, continuing..." -BackgroundColor Green -ForegroundColor Black
+    Write-Output ".OST not found, continuing..." -BackgroundColor Green -ForegroundColor Black
 }
 
 # Test key path
@@ -59,12 +59,12 @@ $TestPath15 = Test-Path -Path $Key15 -IsValid
 # Delete key
 if ($TestPath16 -eq $TRUE) {
     Remove-Item $Key16 -Recurse -EA SilentlyContinue
-    Write-Host "Outlook 2016 profiles are removed." -BackgroundColor Green -ForegroundColor Black
+    Write-Output "Outlook 2016 profiles are removed." -BackgroundColor Green -ForegroundColor Black
 }
 elseif ($TestPath15 -eq $TRUE) {
     Remove-Item $Key15 -Recurse -EA SilentlyContinue
-    Write-Host "Outlook 2013 profiles are removed." -BackgroundColor Green -ForegroundColor Black
+    Write-Output "Outlook 2013 profiles are removed." -BackgroundColor Green -ForegroundColor Black
 }
 else {
-    Write-Host "Outlook 2016 and Outlook 2013 not found." -BackgroundColor Red -ForegroundColor Black
+    Write-Output "Outlook 2016 and Outlook 2013 not found." -BackgroundColor Red -ForegroundColor Black
 }
