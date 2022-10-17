13b33c7f47c42dc38f551348f2e9b6f893e4201f
charliegrahamm <93210017+charliegrahamm@users.noreply.github.com>
2022-10-17 11:42:25 +0100

    new file:   Windows/Active Directory/ExtractUsersWithSpecificUPNFromAD.ps1

---
+++ Windows/Active Directory/ExtractUsersWithSpecificUPNFromAD.ps1
@@ -0,0 +1,57 @@
+<# 
+ ██████╗██╗  ██╗ █████╗ ██████╗ ██╗     ██╗███████╗     ██████╗ ██████╗  █████╗ ██╗  ██╗ █████╗ ███╗   ███╗
+██╔════╝██║  ██║██╔══██╗██╔══██╗██║     ██║██╔════╝    ██╔════╝ ██╔══██╗██╔══██╗██║  ██║██╔══██╗████╗ ████║
+██║     ███████║███████║██████╔╝██║     ██║█████╗      ██║  ███╗██████╔╝███████║███████║███████║██╔████╔██║
+██║     ██╔══██║██╔══██║██╔══██╗██║     ██║██╔══╝      ██║   ██║██╔══██╗██╔══██║██╔══██║██╔══██║██║╚██╔╝██║
+╚██████╗██║  ██║██║  ██║██║  ██║███████╗██║███████╗    ╚██████╔╝██║  ██║██║  ██║██║  ██║██║  ██║██║ ╚═╝ ██║
+ ╚═════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝╚═╝╚══════╝     ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝     ╚═╝
+
+.SYNOPSIS  
+    This script does...
+.DESCRIPTION  
+    <Insert Description>
+.NOTES  
+    File Name  : ScriptName.ps1  
+    Author     : Charlie Graham 
+    Requires   : <Required PS Version, modules etc.>
+#>
+
+# This will self elevate the script with a UAC prompt since this script needs to be run as an Administrator in order to function properly.
+If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]'Administrator')) {
+    Write-Host "You didn't run this script as an Administrator. This script will self elevate to run as an Administrator and continue."
+    Start-Sleep 1
+    Write-Host " Launching in Admin mode" -f DarkRed
+    $pwshexe = (Get-Command 'powershell.exe').Source
+    Start-Process $pwshexe -ArgumentList ("-NoProfile -ExecutionPolicy Bypass -File `"{0}`"" -f $PSCommandPath) -Verb RunAs
+    Exit
+}
+
+# Force TLS 1.2
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+# Allow PSGallery Repository
+Write-Host "Allowing PSGallery" -ForegroundColor Green
+Set-PSRepository -Name 'PSGallery' -InstallationPolicy Trusted
+
+# Install NuGet if not already
+Write-Host "Install NuGet" -ForegroundColor Green
+Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201
+
+# Install ActiveDirectory if not already
+if (Get-Module -ListAvailable -Name ActiveDirectory) {
+    Write-Host "ActiveDirectory exists" -ForegroundColor Green
+    Update-Module -Name ActiveDirectory
+    Import-Module ActiveDirectory
+} 
+else {
+    Write-Host "ActiveDirectory does not exist" -ForegroundColor Red
+    Install-Module -Name ActiveDirectory
+    Import-Module ActiveDirectory
+}
+
+# Extract User Information
+$UPNSuffix = 'SHELLPRO.LOCAL'
+$ExtractedUsers = Get-ADUser -Filter "userPrincipalName -like '*$UPNSuffix'"
+
+# Export to CSV
+Export-CSV -Path "C:\temp\users.csv" -Force
