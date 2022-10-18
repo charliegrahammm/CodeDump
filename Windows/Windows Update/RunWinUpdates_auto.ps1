83ffc336acb0960533b08f67045672578e2d2fef
charliegrahamm <93210017+charliegrahamm@users.noreply.github.com>
2022-10-18 10:47:58 +0100

    renamed:    Windows/RunWinUpdates.ps1 -> Windows/Windows Update/RunWinUpdates.ps1 	new file:   Windows/Windows Update/RunWinUpdates_auto.ps1

---
+++ Windows/Windows Update/RunWinUpdates_auto.ps1
@@ -0,0 +1,65 @@
+<# 
+ ██████╗██╗  ██╗ █████╗ ██████╗ ██╗     ██╗███████╗     ██████╗ ██████╗  █████╗ ██╗  ██╗ █████╗ ███╗   ███╗
+██╔════╝██║  ██║██╔══██╗██╔══██╗██║     ██║██╔════╝    ██╔════╝ ██╔══██╗██╔══██╗██║  ██║██╔══██╗████╗ ████║
+██║     ███████║███████║██████╔╝██║     ██║█████╗      ██║  ███╗██████╔╝███████║███████║███████║██╔████╔██║
+██║     ██╔══██║██╔══██║██╔══██╗██║     ██║██╔══╝      ██║   ██║██╔══██╗██╔══██║██╔══██║██╔══██║██║╚██╔╝██║
+╚██████╗██║  ██║██║  ██║██║  ██║███████╗██║███████╗    ╚██████╔╝██║  ██║██║  ██║██║  ██║██║  ██║██║ ╚═╝ ██║
+ ╚═════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝╚═╝╚══════╝     ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝     ╚═╝
+
+.SYNOPSIS  
+    This script automatically runs Windows Updates. 
+.DESCRIPTION  
+    This script will automatically install any necessary modules, pull down a list of required updates from Microsoft's servers and install them. It may need to be ran multiple times.
+.NOTES  
+    File Name  : RunWinUpdates.ps1  
+    Author     : Charlie Graham 
+    Requires   : PowerShell v2, PSWindowsUpdate
+#>
+
+# This will self elevate the script with a UAC prompt since this script needs to be run as an Administrator in order to function properly.
+If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]'Administrator')) {
+    Write-Host "You didn't run this script as an Administrator. This script will self elevate to run as an Administrator and continue."
+    Start-Sleep 1
+    Write-Host "Launching in Admin mode" -f DarkRed
+    $pwshexe = (Get-Command 'powershell.exe').Source
+    Start-Process $pwshexe -ArgumentList ("-NoProfile -ExecutionPolicy Bypass -File `"{0}`"" -f $PSCommandPath) -Verb RunAs
+    Exit
+}
+
+# Force TLS 1.2
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+# Allow PSGallery Repository
+Set-PSRepository -Name 'PSGallery' -InstallationPolicy Trusted
+
+# Install NuGet if not already
+Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201
+
+# Install PSWindowsUpdate if not already
+if (Get-Module -ListAvailable -Name PSWindowsUpdate) {
+    Write-Host "PSWindowsUpdate Module exists" -ForegroundColor Green
+    Update-Module -Name PSWindowsUpdate
+    Import-Module PSWindowsUpdate
+} 
+else {
+    Write-Host "PSWindowsUpdate does not exist" -ForegroundColor Red
+    Install-Module -Name PSWindowsUpdate
+    Import-Module PSWindowsUpdate
+}
+
+# Clear previous job
+Write-Host "Clearing previous job..."
+Clear-WUJob
+
+# Check for Windows Updates
+Write-Host "Checking for updates..."
+$updates = Get-WindowsUpdate
+Write-Host $updates.count updates available
+if ($updates.Count -eq 0) {
+    Write-Host "No Updates Available"
+    break
+}
+
+# Run Windows Updates and reboot automatically
+Write-Host "Installing updates..." 
+Install-WindowsUpdate -AcceptAll -IgnoreReboot 
