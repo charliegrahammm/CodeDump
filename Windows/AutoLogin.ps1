53626b3600bc9f81d87ed4d7e36703700ab73943
charliegrahamm <93210017+charliegrahamm@users.noreply.github.com>
2022-10-19 10:16:01 +0100

    new file:   Windows/AutoLogin.ps1

---
+++ Windows/AutoLogin.ps1
@@ -0,0 +1,34 @@
+<# 
+ ██████╗██╗  ██╗ █████╗ ██████╗ ██╗     ██╗███████╗     ██████╗ ██████╗  █████╗ ██╗  ██╗ █████╗ ███╗   ███╗
+██╔════╝██║  ██║██╔══██╗██╔══██╗██║     ██║██╔════╝    ██╔════╝ ██╔══██╗██╔══██╗██║  ██║██╔══██╗████╗ ████║
+██║     ███████║███████║██████╔╝██║     ██║█████╗      ██║  ███╗██████╔╝███████║███████║███████║██╔████╔██║
+██║     ██╔══██║██╔══██║██╔══██╗██║     ██║██╔══╝      ██║   ██║██╔══██╗██╔══██║██╔══██║██╔══██║██║╚██╔╝██║
+╚██████╗██║  ██║██║  ██║██║  ██║███████╗██║███████╗    ╚██████╔╝██║  ██║██║  ██║██║  ██║██║  ██║██║ ╚═╝ ██║
+ ╚═════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝╚═╝╚══════╝     ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝     ╚═╝
+
+.SYNOPSIS  
+    This script sets Windows to automatically sign in on boot with the credentials specified.
+.DESCRIPTION  
+    Enter the credentials in the below parameters and run on a target machine to set auto-login. 
+.NOTES  
+    File Name  : AutoLogin.ps1  
+    Author     : Charlie Graham 
+    Requires   : PowerShell v2
+#>
+
+# Enter credentials below
+$DefaultUsername = "your username"
+$DefaultPassword = "your password"
+
+# Registry key path
+$RegPath = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon"
+
+# Set key
+Set-ItemProperty $RegPath "AutoAdminLogon" -Value "1" -type String 
+Set-ItemProperty $RegPath "DefaultUsername" -Value "$DefaultUsername" -type String 
+Set-ItemProperty $RegPath "DefaultPassword" -Value "$DefaultPassword" -type String
+
+# Remove key
+# Remove-ItemProperty $RegPath "AutoAdminLogon" 
+# Remove-ItemProperty $RegPath "DefaultUsername" 
+# Remove-ItemProperty $RegPath "DefaultPassword"
