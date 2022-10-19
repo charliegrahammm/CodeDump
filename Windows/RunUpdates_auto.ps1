cb2cacd75f1f6e31237339174d99fff530e63324
charliegrahamm <93210017+charliegrahamm@users.noreply.github.com>
2022-10-19 13:21:47 +0100

    new file:   Windows/RunUpdates_auto.ps1

---
+++ Windows/RunUpdates_auto.ps1
@@ -0,0 +1,23 @@
+<# 
+ ██████╗██╗  ██╗ █████╗ ██████╗ ██╗     ██╗███████╗     ██████╗ ██████╗  █████╗ ██╗  ██╗ █████╗ ███╗   ███╗
+██╔════╝██║  ██║██╔══██╗██╔══██╗██║     ██║██╔════╝    ██╔════╝ ██╔══██╗██╔══██╗██║  ██║██╔══██╗████╗ ████║
+██║     ███████║███████║██████╔╝██║     ██║█████╗      ██║  ███╗██████╔╝███████║███████║███████║██╔████╔██║
+██║     ██╔══██║██╔══██║██╔══██╗██║     ██║██╔══╝      ██║   ██║██╔══██╗██╔══██║██╔══██║██╔══██║██║╚██╔╝██║
+╚██████╗██║  ██║██║  ██║██║  ██║███████╗██║███████╗    ╚██████╔╝██║  ██║██║  ██║██║  ██║██║  ██║██║ ╚═╝ ██║
+ ╚═════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝╚═╝╚══════╝     ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝     ╚═╝
+
+.SYNOPSIS  
+    This script runs LSUClient_auto.ps1 and RunWinUpdates_auto.ps1.
+.DESCRIPTION  
+    Run this to automatically run LSUClient and Windows Updates.
+.NOTES  
+    File Name  : RunUpdates_auto.ps1  
+    Author     : Charlie Graham 
+    Requires   : PowerShell v2
+#>
+
+# Run LSUClient_auto
+.\Lenovo\LSUClient_auto.ps1
+
+# Run RunWinUpdates_auto
+.\Windows Update\RunWinUpdates_auto.ps1
