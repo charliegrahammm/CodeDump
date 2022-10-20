671e3eb5585f4cf00180604a09433ac5ebb10ef3
charliegrahamm <93210017+charliegrahamm@users.noreply.github.com>
2022-10-20 12:22:08 +0100

    new file:   Windows/Lenovo/Get_LSUClient_PDQ.ps1

---
+++ Windows/Lenovo/Get_LSUClient_PDQ.ps1
@@ -0,0 +1,43 @@
+<# 
+ ██████╗██╗  ██╗ █████╗ ██████╗ ██╗     ██╗███████╗     ██████╗ ██████╗  █████╗ ██╗  ██╗ █████╗ ███╗   ███╗
+██╔════╝██║  ██║██╔══██╗██╔══██╗██║     ██║██╔════╝    ██╔════╝ ██╔══██╗██╔══██╗██║  ██║██╔══██╗████╗ ████║
+██║     ███████║███████║██████╔╝██║     ██║█████╗      ██║  ███╗██████╔╝███████║███████║███████║██╔████╔██║
+██║     ██╔══██║██╔══██║██╔══██╗██║     ██║██╔══╝      ██║   ██║██╔══██╗██╔══██║██╔══██║██╔══██║██║╚██╔╝██║
+╚██████╗██║  ██║██║  ██║██║  ██║███████╗██║███████╗    ╚██████╔╝██║  ██║██║  ██║██║  ██║██║  ██║██║ ╚═╝ ██║
+ ╚═════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝╚═╝╚══════╝     ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝     ╚═╝
+
+.SYNOPSIS  
+    This script will automatically pull Lenovo System Updates from https://download.lenovo.com. To be used as a PDQ PowerShell Scanner.
+.DESCRIPTION  
+    The PDQ version of this script is designed to run as a PDQ PowerShell scanner. The task must be ran as an administrator.
+.NOTES  
+    File Name  : Get_LSUClient_PDQ.ps1  
+    Author     : Charlie Graham 
+    Requires   : PowerShell V2, LSUClient, NuGet
+#>
+
+# Force TLS 1.2
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+# Allow PSGallery Repository
+Set-PSRepository -Name 'PSGallery' -InstallationPolicy Trusted
+
+# Install NuGet if not already
+Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201
+
+# Install LSUClient if not already
+if (Get-Module -ListAvailable -Name LSUClient) {
+    Update-Module -Name LSUClient
+    Import-Module LSUClient
+} 
+else {
+    Install-Module -Name LSUClient
+    Import-Module LSUClient
+}
+
+# Gather updates in a loop
+$updates = Get-LSUpdate | Where-Object { $_.Installer.Unattended } -Verbose
+
+[PSCustomObject]@{
+    UpdateCount     = $updates.Count
+}
