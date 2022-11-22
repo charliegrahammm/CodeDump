2c6292eac1fed270461296d30934b33eef2bab66
Charlie Graham <chariegrahamm2014@gmail.com>
2022-11-22 11:11:57 +0000

    modified:   Windows/Updates/CopyFilesLocally.ps1

--- Windows/Updates/CopyFilesLocally.ps1
+++ Windows/Updates/CopyFilesLocally.ps1
@@ -11,6 +11,8 @@ Copy-Item -Path ".\Lenovo\" -Destination "C:\Temp\Lenovo\" -Recurse -Force
 Copy-Item -Path ".\Windows Update\" -Destination "C:\Temp\Windows Update\" -Recurse -Force
 Copy-Item -Path "RunUpdates_auto.ps1" -Destination "C:\Temp\" -Recurse -Force
 Copy-Item -Path "RunUpdates_auto.bat" -Destination "C:\Temp\" -Recurse -Force
+Copy-Item -Path "RunUpdates_Decrapify_auto.ps1" -Destination "C:\Temp\" -Recurse -Force
+Copy-Item -Path "RunUpdates_Decrapify_auto.bat" -Destination "C:\Temp\" -Recurse -Force
 
 
 
