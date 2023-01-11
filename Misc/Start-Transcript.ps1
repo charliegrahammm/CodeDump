00388cb71589a159bab098d7b0f87163b5b9905f
Charlie Graham <charliegrahamm2014@gmail.com>
2023-01-11 23:35:17 +0000

    modified:   Misc/Start-Transcript.ps1

--- Misc/Start-Transcript.ps1
+++ Misc/Start-Transcript.ps1
@@ -1,3 +1,6 @@
 # Start Transcript
 $TranscriptFilename = "C:\Logs\TranscriptName - $(get-date -f "yyyy-MM-dd HH.mm.ss").txt"
 Start-Transcript -Path $TranscriptFilename
+
+# Stop Transcript
+Stop-Transcript | Out-Null
