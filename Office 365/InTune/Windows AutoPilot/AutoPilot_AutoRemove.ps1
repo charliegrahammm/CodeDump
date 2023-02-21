f45e12aa3215076c4745394de3b4b05c1ef7c134
Charlie Graham <charliegrahamm2014@gmail.com>
2023-02-21 15:30:06 +0000

    modified:   Office 365/InTune/Windows AutoPilot/AutoPilot_AutoRegister.ps1 	modified:   Office 365/InTune/Windows AutoPilot/AutoPilot_AutoRemove.ps1

--- Office 365/InTune/Windows AutoPilot/AutoPilot_AutoRemove.ps1
+++ Office 365/InTune/Windows AutoPilot/AutoPilot_AutoRemove.ps1
@@ -16,16 +16,6 @@
     Requires   : NuGet, PSGallery, Get-WindowsAutopilotInfo
 #>
 
-# This will self elevate the script with a UAC prompt since this script needs to be run as an Administrator in order to function properly.
-If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]'Administrator')) {
-    Write-Host "You didn't run this script as an Administrator. This script will self elevate to run as an Administrator and continue."
-    Start-Sleep 1
-    Write-Host "Launching in Admin mode" -f DarkRed
-    $pwshexe = (Get-Command 'powershell.exe').Source
-    Start-Process $pwshexe -ArgumentList ("-NoProfile -ExecutionPolicy Bypass -File `"{0}`"" -f $PSCommandPath) -Verb RunAs
-    Exit
-}
-
 # Force TLS 1.2
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
