694f36a2e101f6c3538c21a2c5943cd9519d18e0
Charlie Graham <charliegrahamm2014@gmail.com>
2023-03-08 15:37:34 +0000

    new file:   Misc/CycleLogFile.ps1

---
+++ Misc/CycleLogFile.ps1
@@ -0,0 +1,24 @@
+<# 
+ ██████╗██╗  ██╗ █████╗ ██████╗ ██╗     ██╗███████╗     ██████╗ ██████╗  █████╗ ██╗  ██╗ █████╗ ███╗   ███╗
+██╔════╝██║  ██║██╔══██╗██╔══██╗██║     ██║██╔════╝    ██╔════╝ ██╔══██╗██╔══██╗██║  ██║██╔══██╗████╗ ████║
+██║     ███████║███████║██████╔╝██║     ██║█████╗      ██║  ███╗██████╔╝███████║███████║███████║██╔████╔██║
+██║     ██╔══██║██╔══██║██╔══██╗██║     ██║██╔══╝      ██║   ██║██╔══██╗██╔══██║██╔══██║██╔══██║██║╚██╔╝██║
+╚██████╗██║  ██║██║  ██║██║  ██║███████╗██║███████╗    ╚██████╔╝██║  ██║██║  ██║██║  ██║██║  ██║██║ ╚═╝ ██║
+ ╚═════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝╚═╝╚══════╝     ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝     ╚═╝
+
+.SYNOPSIS  
+    This script renames a log file and moves it to a specified location.
+.DESCRIPTION  
+    Renames a specified log file and moves to a specified location. This is especially useful for cycling log files on servers. 
+.NOTES  
+    File Name  : CycleLogFile.ps1  
+    Author     : Charlie Graham 
+    Requires   : <Required PS Version, modules etc.>
+#>
+
+# Specify Locations
+$FileStart = "C:\Logs\patient-management-web-api.log"
+$FileEnd = "C:\Logs\Archived\patient-management-web-api - $(get-date -f "yyyy-MM-dd HH.mm.ss").log.old"
+
+# Rename and move file
+Move-Item -Path $FileStart -Destination $FileEnd
