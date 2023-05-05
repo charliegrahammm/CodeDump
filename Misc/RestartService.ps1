eee7240acd8be86c60b309700b835ce9b748a0d8
Charlie Graham <charliegrahamm2014@gmail.com>
2023-05-05 13:20:28 +0100

    new file:   Misc/RestartService.ps1

---
+++ Misc/RestartService.ps1
@@ -0,0 +1,25 @@
+<# 
+ ██████╗██╗  ██╗ █████╗ ██████╗ ██╗     ██╗███████╗     ██████╗ ██████╗  █████╗ ██╗  ██╗ █████╗ ███╗   ███╗
+██╔════╝██║  ██║██╔══██╗██╔══██╗██║     ██║██╔════╝    ██╔════╝ ██╔══██╗██╔══██╗██║  ██║██╔══██╗████╗ ████║
+██║     ███████║███████║██████╔╝██║     ██║█████╗      ██║  ███╗██████╔╝███████║███████║███████║██╔████╔██║
+██║     ██╔══██║██╔══██║██╔══██╗██║     ██║██╔══╝      ██║   ██║██╔══██╗██╔══██║██╔══██║██╔══██║██║╚██╔╝██║
+╚██████╗██║  ██║██║  ██║██║  ██║███████╗██║███████╗    ╚██████╔╝██║  ██║██║  ██║██║  ██║██║  ██║██║ ╚═╝ ██║
+ ╚═════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝╚═╝╚══════╝     ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝     ╚═╝
+
+.SYNOPSIS  
+    This script restarts a specified service.
+.DESCRIPTION  
+    This script will restart a specified service. Can be used by Task Scheduler to restart services on a set schedule. RestartService.ps1 -Service <servicename>
+.NOTES  
+    File Name  : RestartService.ps1  
+    Author     : Charlie Graham 
+    Requires   : PowerShell v2
+#>
+
+Param(
+   [Parameter(Mandatory=$true)]
+   [string]$Service
+)
+
+# Resart specified service
+Restart-Service -Name $Service -Force
