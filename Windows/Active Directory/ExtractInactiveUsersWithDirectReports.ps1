ae87edf4d0df879844279d3535e0974d06b34a9c
charliegrahammm <charliegrahamm2014@gmail.com>
2024-02-12 15:53:53 +0000

    modified:   Windows/Active Directory/ExtractInactiveUsersWithDirectReports.ps1

--- Windows/Active Directory/ExtractInactiveUsersWithDirectReports.ps1
+++ Windows/Active Directory/ExtractInactiveUsersWithDirectReports.ps1
@@ -10,58 +10,102 @@
     This script extracts a list of users with direct reports from the "Corsham > Users > Left" OU to a csv in C:\Temp.
 .DESCRIPTION  
     This script will give you a list of users who have been disabled, but their direct reports havent had their managers updated. This list should be sent to HR to get them to confirm the replacement manager.
+    Utilises the Get-DirectReport Function found here - https://thesysadminchannel.com/get-direct-reports-in-active-directory-using-powershell-recursive/
 .NOTES  
     File Name  : ExtractInactiveUsersWithDirectReports.ps1  
     Author     : Charlie Graham 
     Requires   : PowerShell v2
 #>
 
-##### READ THIS #######
-
-https://thesysadminchannel.com/get-direct-reports-in-active-directory-using-powershell-recursive/
-
-
-
-
-
-
-$TargetUsers = Get-ADUser -Filter * -SearchBase “OU=Left,OU=Users,OU=Corsham,DC=pharmaxo,DC=local” | Select-Object -ExpandProperty SamAccountName
-#$TargetUser = 'charlie.graham'
-$ExportCSV = 'C:\Temp\InactiveUsersWithDirectReports.csv'
-
-# Install ActiveDirectory Module if it doesnt already exist
+# Import ActiveDirectory Module
 Import-Module ActiveDirectory
 
 # Make C:\Temp directory
 Write-Host "Creating C:\Temp directory..."
 New-Item -Path "C:\" -Name "Temp" -ItemType "Directory" -Force
 
-
-$Properties = @(
-    'Name'
-    'SamAccountName'
-    'Description'
-    'Office'
-    'telephoneNumber'
-    'EmailAddress'
-    'DirectReports'
-)
-
-$ADUser = foreach ( $TargetUser in $TargetUsers ) {
-    Get-ADUser -Identity "$TargetUser" -Properties $Properties
-}
-
-$Results = foreach ( $Report in $ADUser.DirectReports ) {
-    $PropHash = [ordered]@{}
-
-    foreach ( $Prop in ( $Properties | Where-Object { $_ -ne 'DirectReports' } ) ) {
-        $PropHash.Add( $Prop, $ADUser.$Prop )
+Function Get-DirectReport {
+    #requires -Module ActiveDirectory
+     
+    <#
+    .SYNOPSIS
+        This script will get a user's direct reports recursively from ActiveDirectory unless specified with the NoRecurse parameter.
+        It also uses the user's EmployeeID attribute as a way to exclude service accounts and/or non standard accounts that are in the reporting structure.
+      
+    .NOTES
+        Name: Get-DirectReport
+        Author: theSysadminChannel
+        Version: 1.0
+        DateCreated: 2020-Jan-28
+      
+    .LINK
+        https://thesysadminchannel.com/get-direct-reports-in-active-directory-using-powershell-recursive -   
+      
+    .PARAMETER SamAccountName
+        Specify the samaccountname (username) to see their direct reports.
+      
+    .PARAMETER NoRecurse
+        Using this option will not drill down further than one level.
+      
+    .EXAMPLE
+        Get-DirectReport username
+      
+    .EXAMPLE
+        Get-DirectReport -SamAccountName username -NoRecurse
+      
+    .EXAMPLE
+        "username" | Get-DirectReport
+    #>
+     
+        [CmdletBinding()]
+        param(
+            [Parameter(
+                Mandatory = $false,
+                ValueFromPipeline = $true,
+                ValueFromPipelineByPropertyName = $true
+            )]
+     
+            [string]  $SamAccountName,
+     
+            [switch]  $NoRecurse
+        )
+     
+        BEGIN {}
+     
+        PROCESS {
+            $UserAccount = Get-ADUser $SamAccountName -Properties DirectReports, DisplayName
+            $UserAccount | select -ExpandProperty DirectReports | ForEach-Object {
+                $User = Get-ADUser $_ -Properties DirectReports, DisplayName, Title, EmployeeID
+                if ($null -ne $User.EmployeeID) {
+                    if (-not $NoRecurse) {
+                        Get-DirectReport $User.SamAccountName
+                    }
+                    [PSCustomObject]@{
+                        SamAccountName     = $User.SamAccountName
+                        UserPrincipalName  = $User.UserPrincipalName
+                        DisplayName        = $User.DisplayName
+                        Manager            = $UserAccount.DisplayName
+                    }
+                }
+            }
+        }
+     
+        END {}
+     
     }
 
-    $PropHash.Add( 'DirectReport', $Report )
 
-    [pscustomobject]$PropHash
+
+
+
+$TargetUsers = Get-ADUser -Filter * -SearchBase “OU=Left,OU=Users,OU=Corsham,DC=pharmaxo,DC=local” | Select-Object -ExpandProperty SamAccountName
+
+
+$Results = foreach ( $TargetUser in $TargetUsers ) {
+   $TargetUser | Get-DirectReport
 }
 
-$Results |
-Export-Csv -Path $ExportCSV -NoTypeInformation
+$Results | Export-Csv -Path $ExportCSV -NoTypeInformation
+
+
+####### THIS SCRIPT SPITS OUT A LIST OF INACTIVE USERS WITH MANAGERS STILL ASSIGNED. NEED TO FLIP THIS AROUND SO ITS SPITTING OUT PEOPLE THAT HAVE LEFT THAT STILL HAVE DIRECT REPORTS ASSIGNED. ##############
